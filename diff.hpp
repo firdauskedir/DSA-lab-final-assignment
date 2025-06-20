048586115ecee357af04f00a402ef4c3065262c6
Fenetmeng <fenetcris7@gmail.com>
2025-06-21 00:01:47 +0300

    Add files via upload

--- diff.hpp
+++
@@ -1,3 +0,0 @@
-#pragma once
-#include <string>
-void diffCommits(const std::string& hash1, const std::string& hash2);
--- merge.hpp
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <string>
-
-void mergeBranches(const std::string& ourHash, const std::string& theirHash);
